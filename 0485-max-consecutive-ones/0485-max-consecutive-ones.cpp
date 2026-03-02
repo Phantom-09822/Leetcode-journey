6b8f3991924a7ba2c43274f29b0c4761cf6ed663
Arpita Jaiswal <arpitajais0910@gmail.com>
2026-03-02 17:59:13 +0530

    Time: 0 ms (100%), Space: 50.2 MB (33.13%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,10 +6,13 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 cnt++;
+                //if current number is 1, snt increases by 1
                 maxi= max(maxi,cnt);
+//this ensures tat maxi remembers the highest streak we've reached so far
             }
             else{
                 cnt=0;
+                //a 0 breaks the chain of 1s, and it starts counting form 0 again.
             }
         }
         return maxi;
